4c9e5dc480b86fd90ed2497da43c6f2ed2299855
FranMori <francois.moriame@gmail.com>
2023-12-18 18:13:35 +0100

    Tip Calculator

---
+++ 1) Tip Calculator/index.js
@@ -0,0 +1,35 @@
+const inputBill = document.getElementById("inputBill")
+const inputTip = document.getElementById("inputTip")
+const btnCalculate = document.getElementById("btnCalculate")
+const total = document.getElementById("total")
+const totalValue = document.getElementById('totalValue')
+
+
+if (sessionStorage.getItem("bill") === null ) {
+  inputBill.placeholder = 100
+} else {
+  inputBill.placeholder = sessionStorage.getItem("bill")
+
+}
+
+if (sessionStorage.getItem("tip") === null) {
+  inputTip.placeholder = 21
+
+} else {
+  inputTip.placeholder = sessionStorage.getItem("tip")
+
+}
+console.log((totalValue.text))
+
+btnCalculate.addEventListener("click", () => {
+   sessionStorage.clear
+   sessionStorage.setItem('bill',inputBill.value)
+   sessionStorage.setItem('tip', inputTip.value)
+    location.reload() 
+   })
+
+   if (sessionStorage.getItem("bill") && sessionStorage.getItem("tip")){
+    totalValue.innerHTML = (parseInt(sessionStorage.getItem("bill")) + parseInt(sessionStorage.getItem("bill"))/parseInt(sessionStorage.getItem("tip"))).toFixed(2) + "€"
+   } else {
+    totalValue.innerHTML = "0€"
+   }
